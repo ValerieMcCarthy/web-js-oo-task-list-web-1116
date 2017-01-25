d94f03510e94f3bad9ca7bb7f693b9c2cf3bab0b
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2017-01-25 14:49:20 -0500

    limped along

--- js/controllers/lists.controller.js
+++ js/controllers/lists.controller.js
@@ -1,3 +1,38 @@
 'use strict';
 // Lists Controller
 
+class ListsController {
+
+	constructor (){
+		this.$addListForm = $('#add_list')
+		this.$listTitleInput = $('#list_title')
+		this.$selectListMenu = $('#select_list')
+		this.$addTaskForm = $('#add_task')
+		this.$wrapper = $('#wrapper')
+	}
+
+	init (){
+		this.$addTaskForm.hide()
+		this.$addListForm.submit((event) =>{
+			event.preventDefault()
+			let title = this.$listTitleInput.val()
+			let list = new List(title)
+			 debugger
+			list.build()
+			 $(`ul#list-${list.id}`).parent().children('h2').children('button').click(() => {
+        $(`ul#list-${list.id}`).closest('div').remove()
+		})
+
+		this.$addTaskForm.show()
+		})
+
+
+
+	}
+
+
+
+
+}
+
+
